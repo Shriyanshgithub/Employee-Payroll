c149b814b817509ab020627ac738f5338b7ffe03
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-02-25 02:30:32 +0530

    [

---
+++ Employee-Payroll-Project/src/main/java/com/capgemini/Employee_Payroll_Project/EmployeePayrollProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.Employee_Payroll_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollProjectApplication.class, args);
+	}
+
+}
